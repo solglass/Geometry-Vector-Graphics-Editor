07bf1fa6e1a59eca351586b3e41481df5824d573
Sergey <Scorpion5149678@gmail.com>
2020-11-29 17:19:29 +0300

    TryPush

--- Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/Form1.cs
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/Form1.cs
@@ -12,9 +12,25 @@ namespace Geometry_Vector_Graphics_Editor
 {
     public partial class Form1 : Form
     {
+        Bitmap _btm;
+        Graphics _graphics;
+        Pen pen;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle();
+            _graphics.DrawEllipse(pen, rect);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _btm = new Bitmap(pictureBox1.Width, pictureBox1.Width);
+            _graphics = Graphics.FromImage(_btm);
+            pen = new Pen(Color.Black, 3);
         }
     }
 }
