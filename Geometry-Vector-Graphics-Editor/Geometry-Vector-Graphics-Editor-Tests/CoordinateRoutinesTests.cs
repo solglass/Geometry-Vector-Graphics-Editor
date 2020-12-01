c8afe9593c4c3f746004a2acd40d94040f4afc42
Georgy <solglass@yandex.ru>
2020-12-01 18:36:07 +0300

    Square calculation by adjacent / opposite points + tests

--- Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor-Tests/CoordinateRoutinesTests.cs
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor-Tests/CoordinateRoutinesTests.cs
@@ -10,15 +10,26 @@ namespace Geometry_Vector_Graphics_Editor_Tests
         [TestCase(1, 1)]
         [TestCase(2, 2)]
         [TestCase(3, 3)]
-        public void CalculateSquareCoordinatesByTwoPoints(int actualMockNumber, int expectedMockNumber)
+        public void CalculateSquareCoordinatesByTwoAdjacentPoints(int actualMockNumber, int expectedMockNumber)
         {
-            Point[] actual = GetActualSquareCoordinatesByTwoPoints(actualMockNumber);
-            Point[] expected = GetExpectedMockSquareCoordinatesByTwoPoints(expectedMockNumber);
+            Point[] actual = GetActualSquareCoordinatesByTwoAdjacentPoints(actualMockNumber);
+            Point[] expected = GetExpectedMockSquareCoordinatesByTwoAdjacentPoints(expectedMockNumber);
             Assert.AreEqual(expected, actual);
 
         }
 
-        public Point[] GetExpectedMockSquareCoordinatesByTwoPoints(int n)
+        [TestCase(1, 1)]
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        public void CalculateSquareCoordinatesByTwoOppositePoints(int actualMockNumber, int expectedMockNumber)
+        {
+            Point[] actual = GetActualSquareCoordinatesByTwoOppositePoints(actualMockNumber);
+            Point[] expected = GetExpectedMockSquareCoordinatesByTwoOppositePoints(expectedMockNumber);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        public Point[] GetExpectedMockSquareCoordinatesByTwoAdjacentPoints(int n)
         {
             switch (n)
             {
@@ -39,19 +50,64 @@ namespace Geometry_Vector_Graphics_Editor_Tests
             }
         }
 
-        public Point[] GetActualSquareCoordinatesByTwoPoints(int n)
+        public Point[] GetExpectedMockSquareCoordinatesByTwoOppositePoints(int n)
         {
             switch (n)
             {
                 case 1:
                     return
-                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoPoints(new Point(100, 100), new Point(200, 200));
+                        new Point[4] { new Point(100, 100), new Point(200, 100), new Point(200, 200), new Point(100, 200) };
+                case 2:
+                    return 
+                        new Point[4] { new Point(200, 200),  new Point(100, 200), new Point(100, 100), new Point(200, 100) };
+                case 3:
+                    return 
+                        new Point[4] { new Point(0, 0),    new Point(300, 0), new Point(300, 300), new Point(0, 300) };
+
+                //TODO more tests
+                //    case 4:
+                //        return new Point[] { new Point(100, 100) };
+
+                default:
+                    throw new Exception();
+            }
+        }
+
+        public Point[] GetActualSquareCoordinatesByTwoAdjacentPoints(int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    return
+                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoAdjacentPoints(new Point(100, 100), new Point(200, 200));
                 case 2:
                     return
-                     Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoPoints(new Point(200, 200), new Point(100, 100));
+                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoAdjacentPoints(new Point(200, 200), new Point(100, 100));
                 case 3:
                     return
-                        Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoPoints(new Point(300, 0), new Point(0, 300));
+                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoAdjacentPoints(new Point(300, 0)  , new Point(0, 300));
+                //TODO more tests
+                // case 4:
+                //     return Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoPoints(new Point(800, 800), new Point(200, 200));
+
+                default:
+                    throw new Exception();
+            }
+        }
+
+        public Point[] GetActualSquareCoordinatesByTwoOppositePoints(int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    return
+                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoOppositePoints(new Point(100, 100), new Point(200, 200));
+                case 2:
+                    return
+                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoOppositePoints(new Point(200, 200), new Point(100, 100));
+                case 3:
+                    return
+                       Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoOppositePoints(new Point(0, 0)  , new Point(300, 300));
                 //TODO more tests
                 // case 4:
                 //     return Geometry_Vector_Graphics_Editor.CoordinateRoutines.CalculateSquareCoordinatesByTwoPoints(new Point(800, 800), new Point(200, 200));
--- Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/CoordinateRoutines.cs
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/CoordinateRoutines.cs
@@ -4,47 +4,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Geometry_Vector_Graphics_Editor
 {
     public class CoordinateRoutines
     {
 
-        public static Point[] CalculateSquareCoordinatesByTwoPoints(Point Point1, Point Point2)
+        public static Point[] CalculateSquareCoordinatesByTwoAdjacentPoints(Point pt1, Point pt2)
         {
             Point[] arrPoints = new Point[4];
-            Point Point3;
-            Point Point4;
-            double side = Math.Pow(Point2.X - Point1.X, 2) + Math.Pow(Point2.Y - Point1.Y, 2);
-
-            int ae = Point2.Y - Point1.Y;
-            int be = -(Point2.X - Point1.X);
-            int ce = -(Point1.X * Point2.Y - Point1.Y * Point2.X);
-
-            var xleftDown = (int)((ae * (side - ce - be * Point1.Y) + be * be * Point1.X) / (Math.Pow(ae, 2) + Math.Pow(be, 2)));
-            int yleftDown = (ae * Point1.Y - be * Point1.X + be * xleftDown) / ae;
-            var xrightDown = (int)((ae * (side - ce - be * Point2.Y) + be * be * Point2.X) / (Math.Pow(ae, 2) + Math.Pow(be, 2)));
-            int yrightDown = (ae * Point2.Y - be * Point2.X + be * xrightDown) / ae;
+            Point pt3;
+            Point pt4;
+            double side = Math.Pow(pt2.X - pt1.X, 2) + Math.Pow(pt2.Y - pt1.Y, 2);
 
 
-            Point3 = new Point(xrightDown, yrightDown);
-            Point4 = new Point(xleftDown, yleftDown);
+            int ae = pt2.Y - pt1.Y;
+            if (ae != 0)
+            {
+                int be = pt1.X - pt2.X;
+                int ce = pt1.Y * pt2.X - pt1.X * pt2.Y;
 
-            arrPoints[0] = Point1;
-            arrPoints[1] = Point2;
-            arrPoints[2] = Point3;
-            arrPoints[3] = Point4;
+                int xleftDown = (int)((ae * (side - ce - be * pt1.Y) + be * be * pt1.X) / (Math.Pow(ae, 2) + Math.Pow(be, 2)));
+                int yleftDown = (ae * pt1.Y - be * pt1.X + be * xleftDown) / ae;
+                int xrightDown = (int)((ae * (side - ce - be * pt2.Y) + be * be * pt2.X) / (Math.Pow(ae, 2) + Math.Pow(be, 2)));
+                int yrightDown = (ae * pt2.Y - be * pt2.X + be * xrightDown) / ae;
 
-          //  Array.Sort(arrPoints);
+
+                pt3 = new Point(xrightDown, yrightDown);
+                pt4 = new Point(xleftDown, yleftDown);
+
+                arrPoints[0] = pt1;
+                arrPoints[1] = pt2;
+                arrPoints[2] = pt3;
+                arrPoints[3] = pt4;
+
+            }
+            else { throw new DivideByZeroException("Two points must be of different heights!"); }
             return arrPoints;
         }
 
-        public static Point[] CalculateRectangleCoordinatesByTwoPoints(Point Point1, Point Point2)
+        public static Point[] CalculateSquareCoordinatesByTwoOppositePoints(Point pt1, Point pt2)
         {
             Point[] arrPoints = new Point[4];
-            //TODO Implement analytically
-            Array.Sort(arrPoints);
+            Point pt3 = new Point(); 
+            Point pt4 = new Point();
+            int x1 = pt1.X; 
+            int y1 = pt1.Y ;    // First diagonal point
+            int x2 = pt2.Y  ; 
+            int y2 = pt2.Y ;
+            int xc = (x1 + x2) / 2;
+            int yc = (y1 + y2) / 2;    // Center point
+            int xd = (x1 - x2) / 2; 
+            int yd = (y1 - y2) / 2;    // Half-diagonal
+
+            int x3 = xc - yd; 
+            int y3 = yc + xd;    // Third corner
+            int x4 = xc + yd; 
+            int y4 = yc - xd;    // Fourth corner
+            pt3.X = x3;
+            pt3.Y = y3;
+            pt4.X = x4;
+            pt4.Y = y4;
+
+            arrPoints[0] = pt1;
+            arrPoints[1] = pt3;
+            arrPoints[2] = pt2;
+            arrPoints[3] = pt4;
+
             return arrPoints;
+        }
+
+
+        public static Point[] CalculateRectangleCoordinatesByTwoPoints(Point bottomLeft, Point topRight)
+            {
+                Point[] arrPoints = new Point[4];
+                Point topLeft = new Point(bottomLeft.X, topRight.Y);
+                Point bottomRight = new Point(topRight.X, bottomLeft.Y);
+                arrPoints[0] = bottomLeft;
+                arrPoints[1] = topLeft;
+                arrPoints[2] = topRight;
+                arrPoints[3] = bottomRight;
+               // Rectangle.FromLTRB(0, 0, dist, dist);
+                return arrPoints;
+            }
+
+            public static Point[] CalculateRectangularTriangleCoordinatesByTwoPoints(Point bottomLeft, Point topRight)
+            {
+                Point[] arrPoints = new Point[4];
+            //TODO implement
+                return arrPoints;
+            }
         }
     }
-}
+
+
