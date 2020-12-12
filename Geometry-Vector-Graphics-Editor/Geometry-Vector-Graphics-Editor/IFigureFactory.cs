ba6b71f0b4e297e7775c39de3a8789d952082b6c
Georgy <solglass@yandex.ru>
2020-12-12 12:13:38 +0300

    IFigureFactoryInterface

---
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/IFigureFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry_Vector_Graphics_Editor
+{
+    interface IFigureFactory
+    {
+        bool Contains(Point start, Point end, Point checkPoint, double accuracy);
+
+
+        bool CheckInside(double x, double a, double b, double accuracy);
+
+
+    }
+}
