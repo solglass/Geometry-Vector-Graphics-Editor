28555f4831cec29c13460e70bda2aee1c7c60b6c
Zhanna <yellowcucumber27@gmail.com>
2020-12-14 20:24:52 +0300

    Some interface update

---
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/NPolygonFigure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Geometry_Vector_Graphics_Editor
+{
+    class NPolygonFigure
+    {
+        Bitmap tmpBitmap;
+        public ArrayList Points { get; private set; }
+    }
+}
