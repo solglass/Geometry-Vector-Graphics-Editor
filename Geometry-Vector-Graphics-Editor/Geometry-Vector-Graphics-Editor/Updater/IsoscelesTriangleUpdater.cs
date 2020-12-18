bccd174e8b0f96eddba0ec18d96639014aa57551
Georgy <solglass@yandex.ru>
2020-12-18 20:40:02 +0300

    tmp

---
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/Updater/IsoscelesTriangleUpdater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Geometry_Vector_Graphics_Editor.Actors;
+
+namespace Geometry_Vector_Graphics_Editor.Updater
+{
+    public class IsoscelesTriangleUpdater : IUpdater
+    {
+        public void Update(List<PointF> points)
+        {
+            CoordinateRoutines.CalculateIsoscelesTriangleCoordinatesByTwoPoints(points[0], points[1]);
+        }
+    }
+}
---
+++ Geometry-Vector-Graphics-Editor/Geometry-Vector-Graphics-Editor/Updater/RectangularTriangleUpdater.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+using System.Drawing;
+using Geometry_Vector_Graphics_Editor.Actors;
+
+namespace Geometry_Vector_Graphics_Editor.Updater
+{
+    public class RectangularTriangleUpdater : IUpdater
+    {
+        public void Update(List<PointF> points)
+        {
+            CoordinateRoutines.CalculateRectangularTriangleCoordinatesByTwoPoints(points[0], points[1]);
+        }
+    }
+}
